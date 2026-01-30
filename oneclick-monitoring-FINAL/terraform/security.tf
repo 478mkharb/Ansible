5e640073477d17ff1cadf1e13ee338de7ae7c0cc
Mukesh Kharb <478mkharb@gmail.com>
2026-01-30 23:43:11 +0530

    updated files

--- oneclick-monitoring-FINAL/terraform/security.tf
+++ oneclick-monitoring-FINAL/terraform/security.tf
@@ -1,29 +1,42 @@
 resource "aws_security_group" "monitoring_sg" {
-  name   = "monitoring-sg"
-  vpc_id = aws_vpc.this.id
+  name        = "monitoring-sg"
+  description = "Monitoring and app instances"
+  vpc_id      = aws_vpc.this.id
 
+  # SSH ONLY from bastion
+  ingress {
+    description     = "SSH from bastion"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  # Node Exporter (Prometheus pulls)
   ingress {
     description = "Node Exporter"
     from_port   = 9100
     to_port     = 9100
     protocol    = "tcp"
-    self        = true
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
+  # Prometheus UI
   ingress {
-    description = "Prometheus"
+    description = "Prometheus UI"
     from_port   = 9090
     to_port     = 9090
     protocol    = "tcp"
-    self        = true
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
+  # Grafana UI
   ingress {
-    description = "Grafana"
+    description = "Grafana UI"
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    self        = true
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
@@ -31,6 +44,11 @@ resource "aws_security_group" "monitoring_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "monitoring-sg"
+    Project = var.project
   }
 }
 ############################################
