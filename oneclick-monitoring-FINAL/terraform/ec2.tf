95e14a40ee0d6f1fcc274a0f8bb3ef0415c89c33
Mukesh Kharb <478mkharb@gmail.com>
2026-01-30 17:43:37 +0530

    adding tfvars

--- oneclick-monitoring-FINAL/terraform/ec2.tf
+++ oneclick-monitoring-FINAL/terraform/ec2.tf
@@ -1,27 +1,43 @@
+############################################
+# Monitoring EC2 instances (Prometheus + Grafana)
+# HA setup across 2 private subnets
+############################################
+
 resource "aws_instance" "monitoring" {
   count         = 2
   ami           = var.ubuntu_ami
   instance_type = var.instance_type_monitoring
-  subnet_id     = aws_subnet.private_a.id
-  key_name      = var.key_name
 
-  iam_instance_profile = aws_iam_instance_profile.this.name
+  # Distribute instances across subnets
+  subnet_id = element(
+    [aws_subnet.private_a.id, aws_subnet.private_b.id],
+    count.index
+  )
+
+  key_name = var.key_name
+
+  iam_instance_profile = aws_iam_instance_profile.prometheus_profile.name
   vpc_security_group_ids = [aws_security_group.monitoring_sg.id]
 
   tags = {
-    Name    = "monitoring-${count.index}"
+    Name    = "monitoring-${count.index + 1}"
     Role    = "monitoring"
     Project = var.project
   }
 }
 
+############################################
+# Application EC2 instance
+############################################
+
 resource "aws_instance" "app" {
   ami           = var.ubuntu_ami
   instance_type = var.instance_type_app
   subnet_id     = aws_subnet.private_b.id
-  key_name      = var.key_name
 
-  iam_instance_profile = aws_iam_instance_profile.this.name
+  key_name = var.key_name
+
+  iam_instance_profile = aws_iam_instance_profile.prometheus_profile.name
   vpc_security_group_ids = [aws_security_group.monitoring_sg.id]
 
   tags = {
--- oneclick-monitoring-FINAL/terraform/iam.tf
+++ oneclick-monitoring-FINAL/terraform/iam.tf
@@ -1,29 +1,10 @@
-resource "aws_iam_role" "ec2_role" {
+# Fetch existing IAM role
+data "aws_iam_role" "prometheus_role" {
   name = "prometheus-ec2-discovery"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action = "sts:AssumeRole"
-    }]
-  })
 }
 
-resource "aws_iam_role_policy" "ec2_policy" {
-  role = aws_iam_role.ec2_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect   = "Allow"
-      Action   = ["ec2:DescribeInstances"]
-      Resource = "*"
-    }]
-  })
-}
-
-resource "aws_iam_instance_profile" "this" {
-  role = aws_iam_role.ec2_role.name
+# Create instance profile using existing role
+resource "aws_iam_instance_profile" "prometheus_profile" {
+  name = "prometheus-ec2-profile"
+  role = data.aws_iam_role.prometheus_role.name
 }
