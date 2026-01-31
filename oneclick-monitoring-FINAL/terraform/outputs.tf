e6705d055d562b27890c6dae6ae5a45cc31ece6b
Mukesh Kharb <478mkharb@gmail.com>
2026-02-01 00:26:32 +0530

    updated tfvars

--- oneclick-monitoring-FINAL/terraform/outputs.tf
+++ oneclick-monitoring-FINAL/terraform/outputs.tf
@@ -1,10 +1,11 @@
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
+}
+
 output "monitoring_private_ips" {
   value = aws_instance.monitoring[*].private_ip
 }
 
 output "app_private_ip" {
   value = aws_instance.app.private_ip
-}
-output "bastion_public_ip" {
-  value = aws_instance.bastion.public_ip
 }
