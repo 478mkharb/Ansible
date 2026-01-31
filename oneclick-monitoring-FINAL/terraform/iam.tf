04b9d95ba3a840e893f822dd5e2ef2ab54c787a6
Mukesh Kharb <478mkharb@gmail.com>
2026-01-31 18:19:42 +0530

    modified iam.tf

--- oneclick-monitoring-FINAL/terraform/iam.tf
+++ oneclick-monitoring-FINAL/terraform/iam.tf
@@ -1,15 +1,9 @@
-
 # Read existing Prometheus IAM role
-
-
 data "aws_iam_role" "prometheus_role" {
   name = "prometheus-ec2-discovery"
 }
 
-# Create IAM instance profile for EC2
-
-
-resource "aws_iam_instance_profile" "prometheus_profile" {
+# Read existing IAM instance profile
+data "aws_iam_instance_profile" "prometheus_profile" {
   name = "prometheus-ec2-profile"
-  role = data.aws_iam_role.prometheus_role.name
 }
