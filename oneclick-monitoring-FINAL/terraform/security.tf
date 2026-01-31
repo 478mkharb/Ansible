9b437d50c64fb3701737853365cf649e5b24b35d
Mukesh Kharb <478mkharb@gmail.com>
2026-01-31 23:29:05 +0530

    updated tfvars

--- oneclick-monitoring-FINAL/terraform/security.tf
+++ oneclick-monitoring-FINAL/terraform/security.tf
@@ -1,46 +1,22 @@
 ############################################
-# Jenkins Security Group
-############################################
-resource "aws_security_group" "jenkins_sg" {
-  name        = "jenkins-sg"
-  description = "Security group for Jenkins server"
-  vpc_id      = aws_vpc.this.id
-
-  # Jenkins usually does NOT need inbound SSH from the world
-  # Add inbound rules here ONLY if you manually SSH into Jenkins
-
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name    = "jenkins-sg"
-    Project = var.project
-  }
-}
-
-############################################
 # Bastion Security Group
 ############################################
 resource "aws_security_group" "bastion_sg" {
   name        = "bastion-sg"
-  description = "Allow SSH only from Jenkins SG"
+  description = "Allow SSH from Jenkins public IP"
   vpc_id      = aws_vpc.this.id
 
   ingress {
-    description     = "SSH from Jenkins"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.jenkins_sg.id]
+    description = "SSH from Jenkins EC2"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+
+    # CHANGE THIS TO YOUR JENKINS PUBLIC IP
+    cidr_blocks = ["13.235.0.236/32"]
   }
 
   egress {
-    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -54,11 +30,11 @@ resource "aws_security_group" "bastion_sg" {
 }
 
 ############################################
-# Private EC2 Security Group (Monitoring + App)
+# Private EC2 Security Group
 ############################################
 resource "aws_security_group" "private_ec2_sg" {
   name        = "private-ec2-sg"
-  description = "Allow SSH only from Bastion SG"
+  description = "Allow SSH only from Bastion"
   vpc_id      = aws_vpc.this.id
 
   ingress {
@@ -70,7 +46,6 @@ resource "aws_security_group" "private_ec2_sg" {
   }
 
   egress {
-    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
