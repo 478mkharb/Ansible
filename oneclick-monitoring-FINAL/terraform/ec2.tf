3ec054fd1683db50404fffb7bde3418e38d8a90f
Mukesh Kharb <478mkharb@gmail.com>
2026-02-02 16:25:04 +0530

    updated helm.yml

--- oneclick-monitoring-FINAL/terraform/ec2.tf
+++ oneclick-monitoring-FINAL/terraform/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "bastion" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public.id
-  key_name                    = var.key_name
+  key_name                    = aws_key_pair.oneclick.key_name
   associate_public_ip_address = true
 
   vpc_security_group_ids = [
@@ -31,13 +31,12 @@ resource "aws_instance" "monitoring" {
   count         = 2
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type_monitoring
+  key_name      = aws_key_pair.oneclick.key_name
 
   subnet_id = element(
     [aws_subnet.private_a.id, aws_subnet.private_b.id],
     count.index
   )
-
-  key_name = aws_key_pair.oneclick.key_name
 
   iam_instance_profile   = data.aws_iam_instance_profile.prometheus_profile.name
   vpc_security_group_ids = [aws_security_group.private_ec2_sg.id]
