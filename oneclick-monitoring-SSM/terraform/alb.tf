21ce2071a63a5851a9cc56fb43b936b045df6187
Mukesh Kharb <478mkharb@gmail.com>
2026-02-03 16:25:10 +0530

    uploading alb.tf

--- oneclick-monitoring-SSM/terraform/alb.tf
+++ oneclick-monitoring-SSM/terraform/alb.tf
@@ -1,20 +1,54 @@
+############################################
+# ALB Security Group
+############################################
+resource "aws_security_group" "alb_sg" {
+  name        = "monitoring-alb-sg"
+  description = "Allow HTTP to ALB"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "monitoring-alb-sg"
+    Project = var.project
+  }
+}
+
+############################################
+# Application Load Balancer
+############################################
 resource "aws_lb" "monitoring_alb" {
   name               = "monitoring-alb"
+  internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  
+
   subnets = [
-    aws_subnet.public_a.id,
+    aws_subnet.public.id,
     aws_subnet.public_b.id
   ]
 
-  
   tags = {
     Name    = "monitoring-alb"
     Project = var.project
   }
 }
 
+############################################
+# Target Group - Grafana
+############################################
 resource "aws_lb_target_group" "grafana_tg" {
   name        = "grafana-tg"
   port        = 30000
@@ -23,10 +57,19 @@ resource "aws_lb_target_group" "grafana_tg" {
   target_type = "instance"
 
   health_check {
-    path = "/"
+    path    = "/"
+    matcher = "200-399"
+  }
+
+  tags = {
+    Name    = "grafana-tg"
+    Project = var.project
   }
 }
 
+############################################
+# Target Group - Prometheus
+############################################
 resource "aws_lb_target_group" "prometheus_tg" {
   name        = "prometheus-tg"
   port        = 30090
@@ -35,11 +78,19 @@ resource "aws_lb_target_group" "prometheus_tg" {
   target_type = "instance"
 
   health_check {
-    path = "/"
+    path    = "/"
+    matcher = "200-399"
+  }
+
+  tags = {
+    Name    = "prometheus-tg"
+    Project = var.project
   }
 }
 
-
+############################################
+# ALB Listener
+############################################
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.monitoring_alb.arn
   port              = 80
@@ -50,12 +101,15 @@ resource "aws_lb_listener" "http" {
 
     fixed_response {
       content_type = "text/plain"
-      message_body = "Monitoring ALB"
+      message_body = "Monitoring ALB Running"
       status_code  = "200"
     }
   }
 }
 
+############################################
+# Listener Rule - Grafana
+############################################
 resource "aws_lb_listener_rule" "grafana_rule" {
   listener_arn = aws_lb_listener.http.arn
   priority     = 10
@@ -72,6 +126,9 @@ resource "aws_lb_listener_rule" "grafana_rule" {
   }
 }
 
+############################################
+# Listener Rule - Prometheus
+############################################
 resource "aws_lb_listener_rule" "prometheus_rule" {
   listener_arn = aws_lb_listener.http.arn
   priority     = 20
