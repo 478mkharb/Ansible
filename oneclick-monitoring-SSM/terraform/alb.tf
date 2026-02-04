b399aa9dccaaef6de5bcfa380a00d2d3a898dd26
Mukesh Kharb <478mkharb@gmail.com>
2026-02-04 16:56:20 +0530

    Fix invalid stdout_callback for Ansible 2.15

--- oneclick-monitoring-SSM/terraform/alb.tf
+++ oneclick-monitoring-SSM/terraform/alb.tf
@@ -111,18 +111,13 @@ resource "aws_lb_listener" "prometheus_http" {
 # (If using ASG, skip this section)
 ############################################
 
-resource "aws_lb_target_group_attachment" "grafana_attach" {
-  for_each = toset(var.monitoring_instance_ids)
-
-  target_group_arn = aws_lb_target_group.grafana_tg.arn
-  target_id        = each.value
-  port             = 32000
+resource "aws_autoscaling_attachment" "grafana_asg_attach" {
+  autoscaling_group_name = aws_autoscaling_group.monitoring_asg.name
+  lb_target_group_arn    = aws_lb_target_group.grafana_tg.arn
 }
 
-resource "aws_lb_target_group_attachment" "prometheus_attach" {
-  for_each = toset(var.monitoring_instance_ids)
-
-  target_group_arn = aws_lb_target_group.prometheus_tg.arn
-  target_id        = each.value
-  port             = 32090
+resource "aws_autoscaling_attachment" "prometheus_asg_attach" {
+  autoscaling_group_name = aws_autoscaling_group.monitoring_asg.name
+  lb_target_group_arn    = aws_lb_target_group.prometheus_tg.arn
 }
+
