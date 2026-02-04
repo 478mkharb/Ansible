b81d9cb136d1a92a8b1c3c76aa11f7e93f0fdd5e
Mukesh Kharb <478mkharb@gmail.com>
2026-02-05 00:52:37 +0530

    uploading vpc-enpoint

---
+++ oneclick-monitoring-VPC-end/terraform/launch_template.tf
@@ -0,0 +1,23 @@
+resource "aws_launch_template" "monitoring_lt" {
+  name_prefix   = "lt-${var.project}-"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type_monitoring
+
+  iam_instance_profile {
+    name = "ec2-ssm-profile"
+  }
+
+  vpc_security_group_ids = [
+    aws_security_group.private_ec2_sg.id
+  ]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name    = "monitoring-instance"
+      Role    = var.role
+      Project = var.project
+    }
+  }
+}
---
+++ oneclick-monitoring-VPC-end/terraform/vpc-endpoints.tf
@@ -0,0 +1,60 @@
+resource "aws_security_group" "ssm_endpoint_sg" {
+  name   = "ssm-endpoint-sg"
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id            = aws_vpc.this.id
+  service_name      = "com.amazonaws.ap-south-1.ssm"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
+  ]
+
+  security_group_ids  = [aws_security_group.ssm_endpoint_sg.id]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ec2messages" {
+  vpc_id            = aws_vpc.this.id
+  service_name      = "com.amazonaws.ap-south-1.ec2messages"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
+  ]
+
+  security_group_ids  = [aws_security_group.ssm_endpoint_sg.id]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ssmmessages" {
+  vpc_id            = aws_vpc.this.id
+  service_name      = "com.amazonaws.ap-south-1.ssmmessages"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
+  ]
+
+  security_group_ids  = [aws_security_group.ssm_endpoint_sg.id]
+  private_dns_enabled = true
+}
