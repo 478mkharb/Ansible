6ee91f2505ddeaa7a29b34504a1fb0b53e9278d9
Mukesh Kharb <478mkharb@gmail.com>
2026-02-04 22:36:43 +0530

    Fix invalid stdout_callback for Ansible 2.15

--- oneclick-monitoring-SSM/terraform/nacl.tf
+++ oneclick-monitoring-SSM/terraform/nacl.tf
@@ -18,13 +18,22 @@ resource "aws_network_acl_association" "private_b" {
   network_acl_id = aws_network_acl.private_nacl.id
 }
 
-# Inbound: ephemeral ports
+resource "aws_network_acl_rule" "inbound_ephemeral_any" {
+  network_acl_id = aws_network_acl.private_nacl.id
+  rule_number    = 80
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 443
+  to_port        = 443
+}
+
 resource "aws_network_acl_rule" "inbound_ephemeral_any" {
   network_acl_id = aws_network_acl.private_nacl.id
   rule_number    = 90
   protocol       = "tcp"
   rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
+  cidr_block     = "10.0.0.0/16"
   from_port      = 1024
   to_port        = 65535
 }
