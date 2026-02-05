ef25f721a3debff28358c701a31a791e6f4b84d9
Mukesh Kharb <478mkharb@gmail.com>
2026-02-05 10:26:04 +0530

    adding backend.tf

--- oneclick-monitoring-VPC-end/terraform/vpc-endpoints.tf
+++ oneclick-monitoring-VPC-end/terraform/vpc-endpoints.tf
@@ -1,12 +1,13 @@
 resource "aws_security_group" "ssm_endpoint_sg" {
-  name   = "ssm-endpoint-sg"
-  vpc_id = aws_vpc.this.id
+  name        = "ssm-endpoint-sg"
+  description = "Allow private EC2 instances to connect to SSM via VPC endpoints"
+  vpc_id      = aws_vpc.this.id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.private_ec2_sg.id]
   }
 
   egress {
@@ -15,80 +16,51 @@ resource "aws_security_group" "ssm_endpoint_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name    = "ssm-endpoint-sg"
+    Project = var.project
+  }
 }
 
 resource "aws_vpc_endpoint" "ssm" {
-  vpc_id            = aws_vpc.this.id
-  service_name      = "com.amazonaws.ap-south-1.ssm"
-  vpc_endpoint_type = "Interface"
-
-  subnet_ids = [
-    aws_subnet.private_a.id,
-    aws_subnet.private_b.id
-  ]
-
+  vpc_id              = aws_vpc.this.id
+  service_name        = "com.amazonaws.${var.region}.ssm"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private[*].id
   security_group_ids  = [aws_security_group.ssm_endpoint_sg.id]
   private_dns_enabled = true
+
+  tags = {
+    Name    = "ssm-endpoint"
+    Project = var.project
+  }
 }
 
 resource "aws_vpc_endpoint" "ec2messages" {
-  vpc_id            = aws_vpc.this.id
-  service_name      = "com.amazonaws.ap-south-1.ec2messages"
-  vpc_endpoint_type = "Interface"
-
-  subnet_ids = [
-    aws_subnet.private_a.id,
-    aws_subnet.private_b.id
-  ]
-
+  vpc_id              = aws_vpc.this.id
+  service_name        = "com.amazonaws.${var.region}.ec2messages"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private[*].id
   security_group_ids  = [aws_security_group.ssm_endpoint_sg.id]
   private_dns_enabled = true
+
+  tags = {
+    Name    = "ec2messages-endpoint"
+    Project = var.project
+  }
 }
 
 resource "aws_vpc_endpoint" "ssmmessages" {
-  vpc_id            = aws_vpc.this.id
-  service_name      = "com.amazonaws.ap-south-1.ssmmessages"
-  vpc_endpoint_type = "Interface"
-
-  subnet_ids = [
-    aws_subnet.private_a.id,
-    aws_subnet.private_b.id
-  ]
-
+  vpc_id              = aws_vpc.this.id
+  service_name        = "com.amazonaws.${var.region}.ssmmessages"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private[*].id
   security_group_ids  = [aws_security_group.ssm_endpoint_sg.id]
   private_dns_enabled = true
+
+  tags = {
+    Name    = "ssmmessages-endpoint"
+    Project = var.project
+  }
 }
-
-resource "aws_vpc_endpoint" "ssm" {
-  vpc_id            = aws_vpc.this.id
-  service_name      = "com.amazonaws.${var.region}.ssm"
-  vpc_endpoint_type = "Interface"
-
-  subnet_ids         = aws_subnet.private[*].id
-  security_group_ids = [aws_security_group.ssm_endpoint_sg.id]
-
-  private_dns_enabled = true
-}
-
-resource "aws_vpc_endpoint" "ec2messages" {
-  vpc_id            = aws_vpc.this.id
-  service_name      = "com.amazonaws.${var.region}.ec2messages"
-  vpc_endpoint_type = "Interface"
-
-  subnet_ids         = aws_subnet.private[*].id
-  security_group_ids = [aws_security_group.ssm_endpoint_sg.id]
-
-  private_dns_enabled = true
-}
-
-resource "aws_vpc_endpoint" "ssmmessages" {
-  vpc_id            = aws_vpc.this.id
-  service_name      = "com.amazonaws.${var.region}.ssmmessages"
-  vpc_endpoint_type = "Interface"
-
-  subnet_ids         = aws_subnet.private[*].id
-  security_group_ids = [aws_security_group.ssm_endpoint_sg.id]
-
-  private_dns_enabled = true
-}
-
