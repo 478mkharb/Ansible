04f3ae059f21876976d17a59500d0a6f8c0bd376
Mukesh Kharb <478mkharb@gmail.com>
2026-02-05 11:16:14 +0530

    Uploading oneclick-NAT

--- oneclick-monitoring-NAT/terraform/backend.tf
+++ oneclick-monitoring-NAT/terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "oneclick-terraform-state-036253061030"
+    bucket         = "oneclick-terraform-tf-bucket-nat"
     key            = "oneclick-monitoring/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "terraform-locks-nat"
   }
 }
